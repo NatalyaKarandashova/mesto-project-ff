2f69f8fe721923b52d51d91fcbd397a791418b2f
Natalya Karandashova <web.natkarandashova@gmail.com>
2024-11-04 14:16:37 +0300

    версия проекта до первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
+function createCard (cardData, deleteCard) {
 
-// @todo: Функция удаления карточки
+    const cardElem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const imageInCard = cardElem.querySelector('.card__image');
+    const deleteButtonInCard = cardElem.querySelector('.card__delete-button');
+    const descriptionInCard = cardElem.querySelector('.card__description');
 
-// @todo: Вывести карточки на страницу
+    imageInCard.src = cardData.link;
+    imageInCard.alt = cardData.name;
+    descriptionInCard.textContent = cardData.name;
+
+    deleteButtonInCard.addEventListener('click', function() {
+        deleteCard(cardElem);
+    })
+
+    return cardElem;
+}
+
+function deleteCard (cardData) {
+    cardData.remove();
+}
+
+const cardList = document.querySelector('.places__list');
+
+initialCards.forEach((cardData) => {
+    const cardElem = createCard (cardData, deleteCard);
+    cardList.append(cardElem);
+});
